e366f8dfcabdb883b6eccc21185ec3a183a4629a
Якушев Марат <yakushev1396@gmail.com>
2017-04-29 20:03:10 +0300

    First Commit

---
+++ app/src/main/java/com/ulsu/marat/valuter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ulsu.marat.valuter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
