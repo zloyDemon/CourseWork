04be8e5244aa6237914c5f3e16e3923434c9ad42
Якушев Марат <yakushev1396@gmail.com>
2017-05-09 23:25:26 +0300

    #Сделал item для списка, добавил файл dimens, добавил свои шрифты

--- app/src/main/java/com/ulsu/marat/valuter/controller/MainActivity.java
+++ app/src/main/java/com/ulsu/marat/valuter/controller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ulsu.marat.valuter.controller;
 
+import android.content.Context;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,9 @@ import com.ulsu.marat.valuter.controller.adapter.ViewPagerAdapter;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -27,15 +31,26 @@ public class MainActivity extends AppCompatActivity {
     ViewPagerAdapter mViewPagerAdapter;
 
     @AfterViews
-    public void bindViews(){
+    public void bindViews() {
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Geometria.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setSupportActionBar(toolbar);
         mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        mViewPagerAdapter.addFragments(new CurrencyListFragment_(),"");
-        mViewPagerAdapter.addFragments(new CurrencyConverterFragment_(),"");
+        mViewPagerAdapter.addFragments(new CurrencyListFragment_(), "");
+        mViewPagerAdapter.addFragments(new CurrencyConverterFragment_(), "");
 
         mViewPager.setAdapter(mViewPagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
         mTabLayout.getTabAt(0).setIcon(R.drawable.tab_list_icon);
         mTabLayout.getTabAt(1).setIcon(R.drawable.tab_list_icon);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
